6917d1a490194b58e6c441620f4b8b8a484c9cb6
엄태건 <umtimos@naver.com>
2024-02-28 15:23:33 +0900

    엄태건 : 최초 커밋

---
+++ Source/DrawSystem/DrawSystem.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DrawSystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DrawSystem, "DrawSystem" );
